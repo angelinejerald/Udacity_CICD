aa119183acc65c26f0b58037485629f715009561
angelinejerald <jangelinemahiba@gmail.com>
2021-02-19 22:48:46 +0530

    Update cleanup.sh

--- cleanup.sh
+++ cleanup.sh
@@ -1,10 +1,10 @@
 stacks=$(aws cloudformation list-stacks --query "StackSummaries[*].StackName" --stack-status-filter CREATE_COMPLETE --no-paginate --output text)
-AWS_PAGER="" aws cloudformation list-exports --query "Exports[?Name==\`WorkflowID\`].Value" --no-paginate --output text > newid.txt
-new_id=$(cat newid.txt)
+AWS_PAGER="" aws cloudformation list-exports --query "Exports[?Name==\`WorkflowID\`].Value" --no-paginate --output text > newwfid.txt
+new_wf_id=$(cat newwfid.txt)
 
 for i in $stacks
 do
-echo $i | grep -v Cloudfront | grep -v $new_id | awk -F '-' '{print $3}' >> ids.txt
+echo $i | grep -v Cloudfront | grep -v $new_wf_id | awk -F '-' '{print $3}' >> ids.txt
 done
 
 cat ids.txt | sort -u >> oldstacks.txt
@@ -13,8 +13,8 @@ for stack in `cat oldstacks.txt`
 do
 echo "delete s3 $stack"
 aws s3 rm "s3://udapeople-$stack" --recursive
-echo "delete backend $stack"
+echo "delete backend stack $stack"
 aws cloudformation delete-stack --stack-name udapeople-backendStack-$stack
-echo "delete frontend $stack"
+echo "delete frontend stack $stack"
 aws cloudformation delete-stack --stack-name udapeople-frontendStack-$stack
 done
